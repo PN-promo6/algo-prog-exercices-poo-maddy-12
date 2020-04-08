4b5b08ee7612eeeca60f5241fca3e9d5aa931dde
Madina <benaferi-madina@hotmail.fr>
2020-04-08 16:24:19 +0200

    Exercise5.ts

--- Exercice5/exercice5.ts
+++ Exercice5/exercice5.ts
@@ -1,4 +1,11 @@
-
+// Créez une classe représentant une place de concert. Le constructeur de cette classe demande cinq paramètre dont un optionnel.
+// La classe possède une méthode retournant en chaîne de caractères les détails de la place de concert sous forme de HTML.
+// Exemple basique de chaîne de caractères retournée :
+// "<p>Numéro de place : ${ this.username }</p>
+// <p>Heure du concert : ${ this.hours }</p>"
+//
+// Dans un algorithme, créez deux instances de cette classe (la 1ère avec un paramètre optionnel,
+// l’autre sans le paramètre optionnel), et afficher les détails de chaque objet crée.
 
 //CLASS
 class ConcertTicket {
